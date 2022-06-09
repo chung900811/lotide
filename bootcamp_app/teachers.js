ccbcb5c1b5ee3f05e77ff9141445ebd1a56ea16b
terry <fc.ho@hotmail.com>
2022-06-09 16:55:09 +0000

    SQL Injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,13 +14,16 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name= '${process.argv[2] ||'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
 `)
 
+const cohortName = [process.argv[2]||'JUL02']
+
+pool.query(queryString, cohortName)
 .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort} : ${row.teacher}`);
     })
-  });
-
+  })
+  .catch(err => console.error('query error', err.stack));
